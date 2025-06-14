76765039bf62a8cc8d789f9b7b39faeab69aeaea
arovid <pvidor2013@gmail.com>
2025-06-14 15:28:21 +0200

    Working convergence check for RKF45

--- RKF45.m
+++ RKF45.m
@@ -2,12 +2,13 @@ function out = RKF45(fun,tInit,tEnd,yInit,RelTol,AbsTol)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-T = {tInit};  % Grid
-Y = {yInit};  % Vector of the numerical approximation (4th order)
-Z = {yInit};  % Vector of the numerical approximation (5th order)
+T = tInit;  % Grid
+Y = yInit;  % Vector of the numerical approximation (4th order)
+Z = yInit;  % Vector of the numerical approximation (5th order)
 
-t = tInit;
-y = yInit;
+t = T(1);
+y = Y(:,1);
+
 
 h = (tEnd-tInit)/1000; % Guess for initial step size
  
@@ -27,16 +28,16 @@ while t < tEnd
     yTemp = y + h*(25/216*k1 + 1408/2565*k3 + 2197/4104*k4 - 1/5*k5);
     zTemp = y + h*(16/135*k1 + 6656/12825*k3 + 28651/56430*k4 - 9/50*k5 + 2/55*k6);
 
-    e = abs(yTemp-zTemp);               % Error in the step
-    etol = max(RelTol*abs(yTemp),AbsTol);   % Error tolerance defined by user
+    e = abs(yTemp-zTemp);                   % Error in the step
+    etol = RelTol*abs(yTemp) + AbsTol;      % Error tolerance defined by user
 
     if all(e<etol)
         y = yTemp;
         z = zTemp;
         t = t + h;
-        Y{end+1} = y;
-        Z{end+1} = z;
-        T{end+1} = t;
+        Y(:,end+1) = y;
+        Z(:,end+1) = z;
+        T(end+1) = t;
         nsteps = nsteps + 1;
     else
         nfailed = nfailed + 1;
@@ -52,9 +53,12 @@ while t < tEnd
     
 end
 
-out.t = cell2mat(T);
-out.y = cell2mat(Y);
-out.z = cell2mat(Z);
+% out.t = cell2mat(T);
+% out.y = cell2mat(Y);
+% out.z = cell2mat(Z);
+out.t = T;
+out.y = Y;
+out.z = Z;
 out.stats.nfeval = nfeval;
 out.stats.nsteps = nsteps;
 out.stats.nfailed = nfailed;
--- RKF45Convergence.m
+++ RKF45Convergence.m
@@ -10,14 +10,13 @@ u1Init = [1.2; 0.994; 0.994];
 u2dInit = [-1.049357510; -2.0317326295573368357302057924; -2.00158510637908252240537862224];
 ICs = @(i)[u1Init(i); 0; 0; u2dInit(i)];
 
-Tol =  @(i) 1e-2./10.^((i-1)/2);
-Ni = 9;
-Nj = 17;
+Tol =  @(i) 1e-3./10.^((i-1)/5);
+Ni = 16;
+Nj = 16;
 
 RKF45Conv.TwoLoop.endpoints = zeros(4,Ni,Nj);
 RKF45Conv.ThreeLoop.endpoints = zeros(4,Ni,Nj);
 RKF45Conv.FourLoop.endpoints = zeros(4,Ni,Nj);
-
 for i = 1:Ni
     for j = 1:Nj
         % Two loops
@@ -32,7 +31,98 @@ for i = 1:Ni
         RKF45Conv.TwoLoop.endpoints(:,i,j) = TwoLoop.RKF45.y(:,end);
         RKF45Conv.ThreeLoop.endpoints(:,i,j) = ThreeLoop.RKF45.y(:,end);
         RKF45Conv.FourLoop.endpoints(:,i,j) = FourLoop.RKF45.y(:,end);
+        clear TwoLoop ThreeLoop FourLoop
     end
 end
 
+fig1 = figure(1);
+tiledlayout(2,2)
+nexttile
 surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.TwoLoop.endpoints(1,:,:))')
+fontsize(12,"points")
+set(0,'defaulttextinterpreter','latex')
+xlabel('$$ \lg(ATOL) [-]$$')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_1 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.TwoLoop.endpoints(2,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_2 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.TwoLoop.endpoints(3,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_3 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.TwoLoop.endpoints(4,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_4 [-]$$')
+
+
+fig2 = figure(2);
+tiledlayout(2,2)
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.ThreeLoop.endpoints(1,:,:))')
+fontsize(12,"points")
+set(0,'defaulttextinterpreter','latex')
+xlabel('$$ \lg(ATOL) [-]$$')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_1 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.ThreeLoop.endpoints(2,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_2 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.ThreeLoop.endpoints(3,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_3 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.ThreeLoop.endpoints(4,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_4 [-]$$')
+
+fig3 = figure(3);
+fig3.Position = [100 100 800 400];
+tiledlayout(2,2,TileSpacing="tight")
+nexttile
+surf(squeeze(log10(Tol(1:Ni))),squeeze(log10(Tol(1:Nj))),squeeze(RKF45Conv.FourLoop.endpoints(1,:,:))')
+fontsize(12,"points")
+set(0,'defaulttextinterpreter','latex')
+xlabel('$$ \lg(ATOL) [-]$$')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_1 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.FourLoop.endpoints(2,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_2 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.FourLoop.endpoints(3,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_3 [-]$$')
+nexttile
+surf(log10(Tol(1:Ni)),log10(Tol(1:Nj)),squeeze(RKF45Conv.FourLoop.endpoints(4,:,:))')
+fontsize(12,"points")
+xlabel('lg(ATOL) [-]')
+ylabel('lg(RTOL) [-]')
+zlabel('$$u_4 [-]$$')
+
+exportgraphics(fig1, 'Y:\Egyetem\MSc\1Semester\Math\project\Three-Body-Problem\figures\RKF45_Convergence_TwoLoop.pdf', 'ContentType', 'vector');
+exportgraphics(fig2, 'Y:\Egyetem\MSc\1Semester\Math\project\Three-Body-Problem\figures\RKF45_Convergence_ThreeLoop.pdf', 'ContentType', 'vector');
+exportgraphics(fig3, 'Y:\Egyetem\MSc\1Semester\Math\project\Three-Body-Problem\figures\RKF45_Convergence_FourLoop.pdf', 'ContentType', 'vector');
+
+
